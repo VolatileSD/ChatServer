f453eb5118a985657570cae0afd7b1ec654e0e7c
Ana Carvalho <pg25335@alunos.uminho.pt>
2014-12-07 14:24:27 +0000

    command type parsing

--- ChatServer/src/main/java/chatserver/data/Command.java
+++ ChatServer/src/main/java/chatserver/data/Command.java
@@ -1,19 +1,31 @@
 package chatserver.data;
 
+import java.util.*;
+
 public class Command {
 	private final CommandType type;
-    private final Object content;  // careful with mutable objects, such as the byte array
+    private final Object content;
+    private final String command;
+    // careful with mutable objects, such as the byte array
+    public Command(String command) { this.command = command;}
     public Command(CommandType type, Object content) { this.type = type; this.content = content; }
 
-    public CommandType getType(String cmd){
+    public CommandType getType(){
+        /*
+        * Filters commands for the simple client
+        
+        */
+        String cmd= this.command;
         if (cmd ==":cr") {
-            return CommandType.CHANGE_ROOM
+            return CommandType.CHANGE_ROOM;
         }
-    	else if (){}
-    	else if (){}
+    	else if (cmd==":h" || cmd==":help"){
+    	    return CommandType.HELP;
+    	}
     	else{
     	    return CommandType.UNKNOWN
     	}
+    
     }
 
     public Object getContent(){
