d5dbb6ac2131d629c9114a8ff977f4e33322fdae
Ana Carvalho <pg25335@alunos.uminho.pt>
2014-12-08 21:44:01 +0000

    message update

--- ChatServer/src/main/java/chatserver/quasar/User.java
+++ ChatServer/src/main/java/chatserver/quasar/User.java
@@ -8,12 +8,14 @@ import co.paralleluniverse.fibers.io.*;
 
 import chatserver.data.Msg;
 import chatserver.data.MsgType;
+import chatserver.data.Util;
+import chatserver.data.CommandType;
 
 
 public class User extends BasicActor<Msg, Void> {
   static int MAXLEN = 1024;
   
-  final ActorRef room;
+  private ActorRef room;
   final FiberSocketChannel socket;
 
   public User(ActorRef room, FiberSocketChannel socket) { 
@@ -22,55 +24,57 @@ public class User extends BasicActor<Msg, Void> {
   }
 
   protected Void doRun() throws InterruptedException, SuspendExecution { //Exceptions
+    Util util = new Util();
     new LineReader(self(), socket).spawn();
-    room.send(new Msg(MsgType.ENTER, self()));
-    socket.write(ByteBuffer.wrap("Welcome\n".getBytes()));
-   
+    room.send(new Msg(MsgType.ENTER, self(), "userx"));
     while (receive(msg -> {
       try {
       switch (msg.getType()) {
         case DATA:
-          String line = msg.getO().toString();
+          String line = msg.getContent().toString();
           if(line.startsWith(":")){
             String[] parts = line.split(" ");
-            Command cmd = new Command(parts[0]);
-
-            switch (cmd.getType()){
+            switch (util.getCommandType(parts[0])){
               //  HELP, LIST_ROOMS, LIST_USERS, CHANGE_ROOM, LOGIN,LOGOUT, UNKNOWN
               case LIST_ROOMS:
+                break;
               case LIST_USERS:
+                break;
               case LOGIN:
+                break;
               case CHANGE_ROOM:
                 byte[] b ="Changing rooms\n".getBytes();
-                room.send(new Msg(MsgType.LINE,b ));
-                room.send(new Msg(MsgType.LEAVE, self()));
-                ActorRef newroom = new Room("Another").spawn();
-                room=newroom;
-                room.send(new Msg(MsgType.ENTER, self()));
+                room.send(new Msg(MsgType.LINE,null,b ));
+                room.send(new Msg(MsgType.LEAVE, self(),null));
+                ActorRef newroom = new Room().spawn();
+                this.room=newroom;
+                room.send(new Msg(MsgType.ENTER, self(),null));
+                break;
               case HELP:
+                break;
                 //Help command: returns a list of all available commands
               case UNKNOWN:
               byte[] uc = "Unknown command\t".getBytes();
-              room.send(new Msg(MsgType.LINE, uc));
-              room.send(new Msg(MsgType.LINE, msg.getO()));
-              return true;
+              socket.write(ByteBuffer.wrap(uc));
+              socket.write(ByteBuffer.wrap((byte[]) msg.getContent()));
+              break;
                 
             }
           } else{
-            room.send(new Msg(MsgType.LINE, msg.getO()));            
+            room.send(new Msg(MsgType.LINE,null, msg.getContent()));            
           }
           return true;
         case EOF:
         case IOE:
-          room.send(new Msg(MsgType.LEAVE, self()));
+          room.send(new Msg(MsgType.LEAVE, self(),null));
           socket.close();
           return false;
         case LINE:
-            socket.write(ByteBuffer.wrap((byte[]) msg.getO()));
+            socket.write(ByteBuffer.wrap((byte[]) msg.getContent()));
           return true;
       }
       } catch (IOException e) {
-        room.send(new Msg(MsgType.LEAVE, self()));
+        room.send(new Msg(MsgType.LEAVE, self(),"userx"));
       }
       return false;  // stops the actor if some unexpected message is received
     }));
@@ -105,16 +109,16 @@ public class User extends BasicActor<Msg, Void> {
               byte[] ba = new byte[out.remaining()];
               out.get(ba);
               out.clear();
-              dest.send(new Msg(MsgType.DATA, ba));
+              dest.send(new Msg(MsgType.DATA, null,ba));
             }
           }
           if (eof && !in.hasRemaining()) break;
           in.compact();
         }
-        dest.send(new Msg(MsgType.EOF, null));
+        dest.send(new Msg(MsgType.EOF, null,null));
         return null;
       } catch (IOException e) {
-        dest.send(new Msg(MsgType.IOE, null));
+        dest.send(new Msg(MsgType.IOE, null,null));
         return null;
       }
     }
