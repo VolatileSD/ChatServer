5ce006abd0569b768573b715fe7c3da7a36be5ba
Ana Carvalho <pg25335@alunos.uminho.pt>
2014-12-08 21:42:37 +0000

    message update

--- ChatServer/src/main/java/chatserver/data/Msg.java
+++ ChatServer/src/main/java/chatserver/data/Msg.java
@@ -1,15 +1,23 @@
 package chatserver.data;
 
+import co.paralleluniverse.actors.ActorRef;
+
+
 public class Msg {
 	private final MsgType type;
-    private final Object o;  // careful with mutable objects, such as the byte array
-    public Msg(MsgType type, Object o) { this.type = type; this.o = o; }
+	private final ActorRef from;
+    private final Object content;  // careful with mutable objects, such as the byte array
+    public Msg(MsgType type, ActorRef from, Object content) { this.type = type; this.from = from; this.content = content; }
 
     public MsgType getType(){
     	return type;
+    } 
+
+    public ActorRef getFrom(){
+        return from;
     }
 
-    public Object getO(){
-    	return o;
+    public Object getContent(){
+    	return content;
     }
 }
