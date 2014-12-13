6963f4e2eaacdeae5ec66263b461184d12ee4229
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2014-12-13 11:17:01 +0000

    "Notification Manager"

--- ChatServer/src/main/java/chatserver/ChatServer.java
+++ ChatServer/src/main/java/chatserver/ChatServer.java
@@ -4,15 +4,18 @@ import co.paralleluniverse.actors.ActorRef;
 
 import chatserver.rest.ChatServerApplication;
 import chatserver.quasar.Acceptor;
+import chatserver.quasar.NotificationManager;
 import chatserver.quasar.RoomManager;
 
 
 public class ChatServer {
   public static void main(String[] args) throws Exception {
-    int port = 1111; //Integer.parseInt(args[0]);
+    int chatPort = 1111; //Integer.parseInt(args[0]);
+    int notificationPort = 2222;
     ActorRef roomManager = new RoomManager().spawn();
     new ChatServerApplication(roomManager).run(args); // starts rest
-    Acceptor acceptor = new Acceptor(port, roomManager);
+    new NotificationManager(notificationPort).spawn(); // starts notifications
+    Acceptor acceptor = new Acceptor(chatPort, roomManager);
     acceptor.spawn();
     acceptor.join();
     /*
