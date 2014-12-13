44d1434cfb02164c4d7c99ab04c27bb99638a2e0
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2014-12-13 11:51:12 +0000

    "Update"

--- ChatServer/src/main/java/chatserver/quasar/NotificationManager.java
+++ ChatServer/src/main/java/chatserver/quasar/NotificationManager.java
@@ -5,14 +5,27 @@ import co.paralleluniverse.actors.BasicActor;
 import co.paralleluniverse.fibers.SuspendExecution;
 
 public class NotificationManager extends BasicActor<Msg, Void> {
+
     private final int port;
-    
-    public NotificationManager(int port){
+
+    public NotificationManager(int port) {
         this.port = port;
     }
-    
+
     @Override
+    @SuppressWarnings("empty-statement")
     protected Void doRun() throws InterruptedException, SuspendExecution {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }   
+        while (receive(msg -> {
+            switch (msg.getType()) {
+                case CREATE:
+                    return true;
+                case LOGIN:
+                    return true;
+                case REMOVE:
+                    return true;
+            }
+            return false;
+        }));
+        return null;
+    }
 }
