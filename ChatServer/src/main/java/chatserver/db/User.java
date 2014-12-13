075d79e8f88fa2fd3d733350a0c9e56498e48964
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2014-12-13 16:15:21 +0000

    NB Format User.java

--- ChatServer/src/main/java/chatserver/db/User.java
+++ ChatServer/src/main/java/chatserver/db/User.java
@@ -2,27 +2,27 @@ package chatserver.db;
 
 import co.paralleluniverse.actors.*;
 
-
-
-
-
 public class User {
-    private ActorRef ref;
-    private final String name;
-    private final String pass;
 
-    public User(String pass,String name){this.name=name; this.pass=pass;}
-    
-    public void setActorRef(ActorRef newref){
-        this.ref=newref;
-    }
-    
-    public ActorRef getActorRef(){
-        return ref;
-    }
-    
-    public String getPassword(){
-        return pass;
-    }
-    
+   private ActorRef ref;
+   private final String name;
+   private final String pass;
+
+   public User(String pass, String name) {
+      this.name = name;
+      this.pass = pass;
+   }
+
+   public void setActorRef(ActorRef newref) {
+      this.ref = newref;
+   }
+
+   public ActorRef getActorRef() {
+      return ref;
+   }
+
+   public String getPassword() {
+      return pass;
+   }
+
 }
