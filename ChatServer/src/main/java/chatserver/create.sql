41ca7ec0ca0d91fbe6aab2d6bc87d744a8398240
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2014-12-21 12:38:48 +0000

    Add create.sql

---
+++ ChatServer/src/main/java/chatserver/create.sql
@@ -0,0 +1,51 @@
+connect remote:localhost/ChatServer admin admin
+script sql
+begin
+commit
+let a  = ALTER DATABASE CUSTOM useLightweightEdges=true
+#
+# User
+#
+DROP CLASS User
+CREATE CLASS User EXTENDS V
+CREATE PROPERTY User.username string
+CREATE PROPERTY User.password string
+CREATE PROPERTY User.roomLog embeddedmap
+CREATE PROPERTY User.registrationDate datetime
+ 
+CREATE INDEX User.username unique
+ 
+#
+# Message
+#
+DROP CLASS Message
+CREATE CLASS Message EXTENDS V
+CREATE PROPERTY Message.id long
+CREATE PROPERTY Message.message string
+CREATE PROPERTY Message.date datetime
+ 
+CREATE INDEX Message.id unique
+ 
+#
+# Room 
+#
+DROP CLASS Room
+CREATE CLASS Room EXTENDS V
+CREATE PROPERTY Room.name string
+ 
+CREATE INDEX Room.name unique
+ 
+#
+# EDGES
+#
+DROP CLASS Messages
+CREATE CLASS Messages EXTENDS E
+# from Room to Message
+ 
+DROP CLASS PrivateMessages
+CREATE CLASS PrivateMessages EXTENDS E
+# from User to Message
+ 
+ 
+return $e
+end
