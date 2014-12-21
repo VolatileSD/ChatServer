b88b5fca26c9d734f376eeb000149e4dca2026e7
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2014-12-21 21:39:19 +0000

    OrientDB

---
+++ ChatServer/src/main/java/chatserver/quasar/Manager.java
@@ -0,0 +1,98 @@
+package chatserver.quasar;
+
+import chatserver.db.MessageODB;
+import chatserver.db.entity.User;
+import chatserver.db.UserODB;
+import chatserver.db.entity.Message;
+import chatserver.util.Msg;
+import chatserver.util.MsgType;
+import co.paralleluniverse.actors.ActorRef;
+import co.paralleluniverse.actors.BasicActor;
+import co.paralleluniverse.fibers.SuspendExecution;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Manager extends BasicActor<Msg, Void> {
+
+   private final Map<String, ActorRef> users = new HashMap();
+   private final UserODB userODB = new UserODB();
+   private final MessageODB messageODB = new MessageODB();
+
+   @Override
+   @SuppressWarnings("empty-statement")
+   protected Void doRun() throws InterruptedException, SuspendExecution {
+      while (receive(msg -> {
+         User user;
+         String[] parts = (String[]) msg.getContent();
+         switch (msg.getType()) {
+            case CREATE:
+               user = userODB.findByUsername(parts[1]);
+               if (user == null) { // If the username does not exists, creates a new user
+                  userODB.create(parts[1], parts[2]);
+                  msg.getFrom().send(new Msg(MsgType.OK));
+               } else { // if the username is in use, emit a warning
+                  msg.getFrom().send(new Msg(MsgType.INVALID));
+               }
+               return true;
+            case LOGIN:
+               user = userODB.findByUsernameAndPassword(parts[1], parts[2]);
+               if (user != null) {
+                  msg.getFrom().send(new Msg(MsgType.OK, null, null, user.getRid()));
+                  // send the rid to the user to speed future queries
+               } else {
+                  msg.getFrom().send(new Msg(MsgType.INVALID));
+               }
+               return true;
+            case LOGIN_OK:
+               users.put(msg.getFromUsername(), msg.getFrom());
+               return true;
+            case PRIVATE:
+               // parts here is something like: ["usernameTo", "message"]
+               user = userODB.findByUsername(parts[0]);
+               if (user != null) {
+                  msg.getFrom().send(new Msg(MsgType.OK));
+                  Message message = messageODB.create(msg.getFromUsername(), parts[1]);
+                  userODB.addPrivateMessage(user, message);
+                  if (users.containsKey(parts[0])) { // if its logged in
+                     users.get(parts[0]).send(new Msg(MsgType.PRIVATE, null, msg.getFromUsername(), null));
+                  }
+               } else {
+                  msg.getFrom().send(new Msg(MsgType.INVALID));
+               }
+               return true;
+            case INBOX:
+               List<Message> inbox = userODB.getInbox(parts[0]);
+               msg.getFrom().send(new Msg(MsgType.OK, null, null, inbox));
+               return true;
+            /*
+             case REMOVE:
+             // authenticate first
+             return true;
+             case LINE:
+             rooms.get(parts[0]).addMessage(msg.getFromUsername(), parts[1]);
+             return true;
+             case HISTORY_ENTRY:
+             // when someone enters a room we add to the user's history the message number of that room
+             // same with leave
+             users.get(msg.getFromUsername()).addHistoryEntry(parts[0], Integer.valueOf(parts[1]));
+             return true;
+             case HISTORY:
+             // give him a clone of the room messages
+             // prevent concurrent exception
+             String roomName = (String) msg.getContent();
+             if (rooms.containsKey(roomName)) {
+             List<MessageDB> messages = rooms.get(roomName).getMessages();
+             List<Integer> roomLog = users.get(msg.getFromUsername()).getRoomLog(roomName);
+             new HistoryWorker(msg.getFrom(), roomLog, messages).spawn();
+             } else {
+             msg.getFrom().send(new Msg(MsgType.INVALID));
+             }
+             return true;
+             */
+         }
+         return false;
+      }));
+      return null;
+   }
+}
