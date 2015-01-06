f4b086616b25273789c1f9548fb9d1a4b62efa6f
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2015-01-06 23:49:13 +0000

    Update Saying

--- Common/src/main/java/common/saying/Saying.java
+++ Common/src/main/java/common/saying/Saying.java
@@ -50,11 +50,11 @@ public class Saying {
       return "Unknown Command.\n";
    }
    
-   public static String getPrivateOk(){
-      return new StringBuilder("Message successfully sent to @").toString();
+   public static String getPrivateOk(String username){
+      return new StringBuilder("Message successfully sent to @").append(username).append("\n").toString();
    }
    
-   public static String getPrivateInvalid(){
-      return new StringBuilder("Unknown user @").toString();
+   public static String getPrivateInvalid(String username){
+      return new StringBuilder("Unknown user @").append(username).append("\n").toString();
    }
 }
