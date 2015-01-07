9c6b075ddfea8a22b87490f3700e58a1e5cf0f0b
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2015-01-07 19:54:04 +0000

    Add make admin and remove admin

--- ChatClient/src/main/java/chatclient/AdminSettings.java
+++ ChatClient/src/main/java/chatclient/AdminSettings.java
@@ -5,8 +5,6 @@ import common.representation.UserRepresentation;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import org.apache.http.HttpResponse;
@@ -59,6 +57,10 @@ public class AdminSettings extends javax.swing.JFrame {
       makeAdminBtn = new javax.swing.JButton();
       makeAdminTxt = new javax.swing.JTextField();
       makeAdminLbl = new javax.swing.JLabel();
+      jPanel3 = new javax.swing.JPanel();
+      removeAdminBtn = new javax.swing.JButton();
+      removeAdminLbl = new javax.swing.JLabel();
+      removeAdminTxt = new javax.swing.JTextField();
 
       setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -91,7 +93,7 @@ public class AdminSettings extends javax.swing.JFrame {
             .addComponent(deleteRoomLbl)
             .addGap(40, 40, 40)
             .addComponent(deleteRoomNameTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
             .addComponent(deleteRoomBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addGap(35, 35, 35))
       );
@@ -121,7 +123,45 @@ public class AdminSettings extends javax.swing.JFrame {
       });
 
       makeAdminLbl.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
-      makeAdminLbl.setText("Make User Admin");
+      makeAdminLbl.setText("Make Admin");
+
+      jPanel3.setBackground(new java.awt.Color(0, 0, 0));
+      jPanel3.setForeground(new java.awt.Color(255, 255, 255));
+
+      removeAdminBtn.setText("Remove");
+      removeAdminBtn.addActionListener(new java.awt.event.ActionListener() {
+         public void actionPerformed(java.awt.event.ActionEvent evt) {
+            removeAdminBtnActionPerformed(evt);
+         }
+      });
+
+      removeAdminLbl.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
+      removeAdminLbl.setForeground(new java.awt.Color(255, 255, 255));
+      removeAdminLbl.setText("Remove Admin");
+
+      javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+      jPanel3.setLayout(jPanel3Layout);
+      jPanel3Layout.setHorizontalGroup(
+         jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+            .addContainerGap()
+            .addComponent(removeAdminLbl)
+            .addGap(29, 29, 29)
+            .addComponent(removeAdminTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(removeAdminBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(35, 35, 35))
+      );
+      jPanel3Layout.setVerticalGroup(
+         jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+         .addGroup(jPanel3Layout.createSequentialGroup()
+            .addGap(29, 29, 29)
+            .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+               .addComponent(removeAdminTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+               .addComponent(removeAdminLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+               .addComponent(removeAdminBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addContainerGap(23, Short.MAX_VALUE))
+      );
 
       javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
       jPanel1.setLayout(jPanel1Layout);
@@ -139,11 +179,12 @@ public class AdminSettings extends javax.swing.JFrame {
                   .addComponent(addRoomBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
                .addGroup(jPanel1Layout.createSequentialGroup()
                   .addComponent(makeAdminLbl)
-                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                  .addGap(48, 48, 48)
                   .addComponent(makeAdminTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
+                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                   .addComponent(makeAdminBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)))
             .addGap(35, 35, 35))
+         .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
       );
       jPanel1Layout.setVerticalGroup(
          jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -160,7 +201,9 @@ public class AdminSettings extends javax.swing.JFrame {
                .addComponent(makeAdminTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                .addComponent(makeAdminLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
                .addComponent(makeAdminBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addContainerGap(125, Short.MAX_VALUE))
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addContainerGap())
       );
 
       javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -185,24 +228,40 @@ public class AdminSettings extends javax.swing.JFrame {
    }// </editor-fold>//GEN-END:initComponents
 
     private void addRoomBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addRoomBtnActionPerformed
-       if (addRoomNameTxt.getText().matches("^[^\\d\\s]+$")) {
-          addRoomRequest(addRoomNameTxt.getText());
+       String roomName = addRoomNameTxt.getText();
+       if (roomName.matches("^[^\\d\\s]+$")) {
+          addRoomRequest(roomName);
        } else {
           errorBox("No spaces or numbers allowed");
        }
     }//GEN-LAST:event_addRoomBtnActionPerformed
 
     private void deleteRoomBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteRoomBtnActionPerformed
-       if (deleteRoomNameTxt.getText().matches("^[^\\d\\s]+$")) {
-          deleteRoomRequest(deleteRoomNameTxt.getText());
+       String roomName = addRoomNameTxt.getText();
+       if (roomName.matches("^[^\\d\\s]+$")) {
+          deleteRoomRequest(roomName);
        } else {
           errorBox("No spaces or numbers allowed");
        }
     }//GEN-LAST:event_deleteRoomBtnActionPerformed
 
    private void makeAdminBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_makeAdminBtnActionPerformed
-      // TODO add your handling code here:
+      String adminUsername = makeAdminTxt.getText();
+      if (!adminUsername.isEmpty()) {
+         makeAdminRequest(adminUsername);
+      } else {
+         errorBox("Insert the admin's username");
+      }
    }//GEN-LAST:event_makeAdminBtnActionPerformed
+
+   private void removeAdminBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removeAdminBtnActionPerformed
+      String adminUsername = removeAdminTxt.getText();
+      if (!adminUsername.isEmpty()) {
+         removeAdminRequest(adminUsername);
+      } else {
+         errorBox("Insert the admin's username");
+      }
+   }//GEN-LAST:event_removeAdminBtnActionPerformed
 
    public void addRoomRequest(String roomName) {
       CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -270,6 +329,71 @@ public class AdminSettings extends javax.swing.JFrame {
          errorBox(e.getMessage());
       }
    }
+   
+   public void makeAdminRequest(String adminUsername) {
+      CloseableHttpClient httpclient = HttpClients.createDefault();
+      try {
+         try {
+            HttpPut httpPut = new HttpPut(new StringBuilder(URL).append("admin/").append(adminUsername).toString());
+            httpPut.addHeader("Volatile-ChatServer-Auth", authEncoded);
+
+            ResponseHandler responseHandler = (final HttpResponse response) -> {
+               int status = response.getStatusLine().getStatusCode();
+               if (status == Status.CREATED.getStatusCode()) {
+                  infoBox("Admin successfully created.");
+                  makeAdminTxt.setText("");
+               } else if (status == Status.UNAUTHORIZED.getStatusCode()) { // which won't happen
+                  infoBox("Somehow you're not an administrator");
+               } else if (status == Status.NOT_FOUND.getStatusCode()) {
+                  infoBox("Username not found.");
+               } else {
+                  errorBox("Unexpected response status: " + status);
+               }
+
+               return status;
+            };
+
+            httpclient.execute(httpPut, responseHandler);
+         } finally {
+            httpclient.close();
+         }
+      } catch (IOException e) {
+         errorBox(e.getMessage());
+      }
+
+   }
+
+   public void removeAdminRequest(String adminUsername) {
+      CloseableHttpClient httpclient = HttpClients.createDefault();
+      try {
+         try {
+            HttpDelete httpDelete = new HttpDelete(new StringBuilder(URL).append("admin/").append(adminUsername).toString());
+            httpDelete.addHeader("Volatile-ChatServer-Auth", authEncoded);
+
+            ResponseHandler responseHandler = (final HttpResponse response) -> {
+               int status = response.getStatusLine().getStatusCode();
+               if (status == Status.OK.getStatusCode()) {
+                  infoBox("Admin successfully removed.");
+                  removeAdminTxt.setText("");
+               } else if (status == Status.UNAUTHORIZED.getStatusCode()) { // which won't happen
+                  infoBox("Somehow you're not an administrator");
+               } else if (status == Status.NOT_FOUND.getStatusCode()) {
+                  infoBox("Admin not found.");
+               } else {
+                  errorBox("Unexpected response status: " + status);
+               }
+
+               return status;
+            };
+
+            httpclient.execute(httpDelete, responseHandler);
+         } finally {
+            httpclient.close();
+         }
+      } catch (IOException e) {
+         errorBox(e.getMessage());
+      }
+   }
 
    private static void errorBox(String errorMessage) {
       JOptionPane.showMessageDialog(null, errorMessage, "Something Went Wrong", JOptionPane.ERROR_MESSAGE);
@@ -289,8 +413,12 @@ public class AdminSettings extends javax.swing.JFrame {
    private javax.swing.Box.Filler filler1;
    private javax.swing.JPanel jPanel1;
    private javax.swing.JPanel jPanel2;
+   private javax.swing.JPanel jPanel3;
    private javax.swing.JButton makeAdminBtn;
    private javax.swing.JLabel makeAdminLbl;
    private javax.swing.JTextField makeAdminTxt;
+   private javax.swing.JButton removeAdminBtn;
+   private javax.swing.JLabel removeAdminLbl;
+   private javax.swing.JTextField removeAdminTxt;
    // End of variables declaration//GEN-END:variables
 }
